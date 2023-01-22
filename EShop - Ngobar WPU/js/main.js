8c377e00dfa24d01f276131ddc3303becf5aa51c
rizal-haw <rizalhidayat006@gmail.com>
2023-01-22 22:28:16 +0700

    Update Hero Section

--- EShop - Ngobar WPU/js/main.js
+++ EShop - Ngobar WPU/js/main.js
@@ -1,6 +1,15 @@
 // Toggle class active
 const navbarNav = document.querySelector('.navbar-nav');
-// Ketika Hamburger Menu Diklik
+// Ketika Hmburger Menu dilkik
 document.querySelector('#hamburger-menu').onclick = () => {
     navbarNav.classList.toggle('active');
-};
+}
+// Klik di luar humberger untuk menghilangkan nav
+
+const humberger = document.querySelector('#humberger-menu');
+
+document.addEventListener('click', function(e) {
+    if (!humberger.contains(e.target) && !navbarNav.contains(e.target)) {
+        navbarNav.classList.remove('active');
+    }
+});
